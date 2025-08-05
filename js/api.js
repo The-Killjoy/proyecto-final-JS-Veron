b284feb6992249d0ee8a543b70626b6cf228d460
The-Killjoy <maxi.689@hotmail.com>
2025-08-05 09:55:13 -0300

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,6 +1,6 @@
 export async function fetchCards() {
   try {
-    const res = await fetch('./data/cartas.json');
+    const res = await fetch('../data/cartas.json');
     if (!res.ok) throw new Error('Error al cargar datos');
     return await res.json();
   } catch (err) {
